0f6dd86849e38a024af168086d1311cf72205064
Sererim <danielleymanw@gmail.com>
2023-03-23 22:16:12 +0300

    Class calculator works perfectly.

--- Calculator_Java_6_OOP/Calculator.py
+++ Calculator_Java_6_OOP/Calculator.py
@@ -1,9 +1,71 @@
 class Calculator:
-    pass
+    x: float
+    y: float
+
+    def __init__(self, x: float = None, y: float = None) -> None:
+        if x is None and y is None:
+            self.x = 0.0
+            self.y = 0.0
+        elif x is not None and y is None:
+            self.x = x
+            self.y = 0.0
+        elif x is None and y is not None:
+            self.x = 0.0
+            self.y = y
+        else:
+            self.x = x
+            self.y = y
+
+    def __str__(self) -> str:
+        foo: str = ""
+        if self.y > 0:
+            foo = f"{self.x} + i{self.y}"
+        elif self.y < 0:
+            foo = f"{self.x} - i{self.y}"
+        else:
+            foo = f"{self.x}"
+        return foo
+
+
+class Add:
+    @staticmethod
+    def add(z1: Calculator, z2: Calculator):
+        pass
+
+
+class Mod:
+    @staticmethod
+    def mod(z1: Calculator):
+        pass
+
+
+class Conjugate:
+    @staticmethod
+    def conjugate(z1: Calculator):
+        pass
+
+
+class Subtract:
+    @staticmethod
+    def sub(z1: Calculator):
+        pass
+
+
+class Multiply:
+    @staticmethod
+    def mulp(z1: Calculator):
+        pass
+
+
+class Divide:
+    @staticmethod
+    def div(z1: Calculator):
+        pass
 
 
 def main() -> None:
-    pass
+    calc = Calculator(0 , 5)
+    print(calc)
 
 
 if __name__ == "__main__":
