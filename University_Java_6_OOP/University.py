24a18ca12eda279bc69255a91d21dc066eaed478
Sererim <danielleymanw@gmail.com>
2023-03-24 17:08:03 +0300

    Bones of the project.

---
+++ University_Java_6_OOP/University.py
@@ -0,0 +1,34 @@
+class Person:
+    pass
+
+
+class University:
+    pass
+
+
+class SaveData:
+    pass
+
+
+class ReadData:
+    pass
+
+
+class GetID:
+    pass
+
+
+class SearchInData:
+    pass
+
+
+class GetData:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
